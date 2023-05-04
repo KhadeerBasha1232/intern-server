eab39385ad569b4442851d72082c7dcf1f856af9
KhadeerBasha1232 <hlobhai041@gmail.com>
2023-05-04 20:16:38 +0530

    added pdf option

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const app = express()
 const port = 5000
 const cors = require("cors")
 const {MongoClient} = require('mongodb')
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+
 
 app.use(express.json())
 app.use(cors({
